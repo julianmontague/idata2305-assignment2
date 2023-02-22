26cf3eac6a1d18b18671a0dbd2767a4e9f2b0847
Julian Montague <juliangm@stud.ntnu.no>
2023-02-22 13:16:40 +0100

    Task 2: Add MovieTicketClient

--- src/main/java/no/ntnu/idata2305/juliangm/assignment2/booking/MovieTicketClient.java
+++ src/main/java/no/ntnu/idata2305/juliangm/assignment2/booking/MovieTicketClient.java
@@ -1,4 +1,22 @@
 package no.ntnu.idata2305.juliangm.assignment2.booking;
 
-public class MovieTicketClient {
+/**
+ *
+ * @author julian
+ */
+public class MovieTicketClient extends Thread {
+    String customerName;
+    int numberOfTickets;
+    MovieTicketServer server;
+
+    public MovieTicketClient(String name, int tickets, MovieTicketServer movieServer) {
+        this.customerName = name;
+        this.numberOfTickets = tickets;
+        this.server = movieServer;
+    }
+
+    @Override
+    public void run() {
+        this.server.bookTicket(this.customerName, this.numberOfTickets);
+    }
 }
