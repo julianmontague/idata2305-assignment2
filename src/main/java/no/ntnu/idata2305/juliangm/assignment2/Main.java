3fde9df2dd4e13be8008a4ed50294c62b6b51c44
Julian Montague <juliangm@stud.ntnu.no>
2023-02-22 12:41:23 +0100

    Initial commit

---
+++ src/main/java/no/ntnu/idata2305/juliangm/assignment2/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package no.ntnu.idata2305.juliangm.assignment2;
+
+/**
+ *
+ * @author julian
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
